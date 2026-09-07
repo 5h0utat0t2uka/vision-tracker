8bfcbab4b65edbf6d23682b941a471c9da5c519b
5h0utat0t2uka <37361670+5h0utat0t2uka@users.noreply.github.com>
2026-09-08 01:13:21 +0900

    fix: update media processing script to use cp instead of copyFile

--- scripts/copy-mediapipe-wasm.mjs
+++ scripts/copy-mediapipe-wasm.mjs
@@ -1,9 +1,17 @@
-import { copyFile, mkdir, readdir } from 'node:fs/promises'
+// import { copyFile, mkdir, readdir } from 'node:fs/promises'
+
+// const sourceDirectory = new URL('../node_modules/@mediapipe/tasks-vision/wasm/', import.meta.url)
+// const targetDirectory = new URL('../public/mediapipe/wasm/', import.meta.url)
+
+// await mkdir(targetDirectory, { recursive: true })
+// for (const filename of await readdir(sourceDirectory)) {
+//   await copyFile(new URL(filename, sourceDirectory), new URL(filename, targetDirectory))
+// }
+import { cp, mkdir, rm } from 'node:fs/promises'
 
 const sourceDirectory = new URL('../node_modules/@mediapipe/tasks-vision/wasm/', import.meta.url)
 const targetDirectory = new URL('../public/mediapipe/wasm/', import.meta.url)
 
+await rm(targetDirectory, { recursive: true, force: true })
 await mkdir(targetDirectory, { recursive: true })
-for (const filename of await readdir(sourceDirectory)) {
-  await copyFile(new URL(filename, sourceDirectory), new URL(filename, targetDirectory))
-}
+await cp(sourceDirectory, targetDirectory, { recursive: true })
