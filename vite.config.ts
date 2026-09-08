3066b51cdadc53f90dd932bd55a5143550aa2f57
5h0utat0t2uka <37361670+5h0utat0t2uka@users.noreply.github.com>
2026-09-09 07:39:03 +0900

    chore: update dependencies and refactor Vite configuration for React support

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+// import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+// import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // plugins: [
+  //   react(),
+  //   babel({ presets: [reactCompilerPreset()] }),
+  // ],
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
+    react({ compiler: true }),
   ],
   server: {
     watch: {
